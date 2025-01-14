851380be73a6e58822876c38c7e4234f2c7b2875
tcrombie <tim.crombie@gmail.com>
2025-01-14 14:31:51 -0500

    fig 4 again

--- code/Figure_4.R
+++ code/Figure_4.R
@@ -66,36 +66,58 @@ or4adf2 <- data.table::rbindlist(or4a2$orthregs)
 or4a2$plots[[3]]
 print(glue::glue("slope = {round(or4adf2[3]$orth.reg.slope, digits = 2)}, y-intercept = {round(or4adf2[3]$orth.reg.intercept, digits = 2)}, r^2 = {round(or4adf2[3]$orth.reg.r.squared, digits = 2)}"))
 
-
-
-
 # need to add option to color QC fail data or filter QC fail data to orthReg function. - DONE, QC filter makes fit worse
 # this will help visualize issues with the Boyd data
 # consider adding a QC filter to enviroTox DB if endpoint changed? and flag that too?
 # add notes to EHP manuscript DB in notion
 
-
-# run regression to NEMATODE_COPAS1
-or4a <- pwOrthReg(data = df4a, group = "group", limit.comp = "NEMATODE_COPAS1", min.n = 5, message = F, plot = T)
-or4adf <- data.table::rbindlist(or4a$orthregs)
-print(glue::glue("slope = {round(or4adf[1]$orth.reg.slope, digits = 2)}, y-intercept = {round(or4adf[1]$orth.reg.intercept, digits = 2)}, r^2 = {round(or4adf[1]$orth.reg.r.squared, digits = 2)}"))
-or4ap <- or4a$plots[[1]]
-new_chems <- or4a$plots[[1]]$data %>% dplyr::pull(chem_name)
-
 #===========================================================#
-# 4c
-# Widmayer vs INVERTEBRATES
+# 4b
+# Widmayer vs INVERTEBRATES 
 # The Invert mortality data can sometimes be labelled as immobility or immobilization or survival select
-# LC50 and EC50 could both be used b/c of immobilization endppoint which is essentailly mortality
+# LC50 and EC50 could both be used b/c of immobilization endppoint which is essentially mortality
 # plot comps like in 4b above and update scott
-df3c <- dat %>%
-  dplyr::filter(group == "NEMATODE" | group == "INVERT")
+df4b <- dat %>%
+  dplyr::filter(source %in% c("Widmayer et al. 2022", "Boyd et al. 2016", "EnviroTox DB")) %>%
+  dplyr::filter(group == "NEMATODE" | group == "INVERT" | group == "NEMATODE_COPAS1") %>%
+  dplyr::filter(case_when(group == "NEMATODE" ~ T,
+                          group == "NEMATODE_COPAS1" ~ T,
+                          source == "EnviroTox DB" & test_statistic %in% c("LC50", "EC50") ~ T, # removing EC50s has a big filtering effect, even though some are coded as mortality
+                          TRUE ~ F)) %>%
+  dplyr::mutate(endpoint2 = dplyr::case_when(endpoint %in% c("Mortality/Growth",
+                                                             "Mortality, Mortality",
+                                                             "Mortality, Survival") ~ "Mortality",
+                                             endpoint == "Immobilization: Change in the failure to respond or lack of movement after mechanical stimulation." ~ "Immobility",
+                                             endpoint == "Intoxication, Immobile" ~ "Immobility",
+                                             TRUE ~ endpoint)) %>%
+  dplyr::mutate(endpoint = endpoint2) %>%
+  dplyr::select(-endpoint2) %>%
+  dplyr::filter(case_when(source == "EnviroTox DB" & !(endpoint %in% c("Mortality", "Immobility"))  ~ F, # take either Mort or immobilization
+                           TRUE ~ T)) %>%
+  dplyr::mutate(endpoint = case_when(source == "EnviroTox DB" & endpoint %in% c("Mortality", "Immobility")  ~ "Mortality",
+                                      TRUE ~ endpoint), # set both to mort
+                 test_statistic = case_when(source == "EnviroTox DB"  ~ "LC50",
+                                      TRUE ~ test_statistic)) # set test_stat to LC50 for both
+  # Some improvement to merge EC50 Immobilization to LC50 Mort - 13 to 17 compounds max at LC50 Mort 2day, with imporved R^2
 
-# run the orth regressions across all pairs
-or3c <- pwOrthReg(data = df3c, group = "group", limit.comp = "NEMATODE", min.n = 5, message = F, plot = T)
-or3cdf <- data.table::rbindlist(or3c$orthregs)
-or3cp <- or3c$plots[[2]]
-print(glue::glue("slope = {round(or3cdf[2]$orth.reg.slope, digits = 2)}, y-intercept = {round(or3cdf[2]$orth.reg.intercept, digits = 2)}, r^2 = {round(or3cdf[2]$orth.reg.r.squared, digits = 2)}"))
+# run the orth regressions across all pairs without QC filter
+or4b <- pwOrthReg(data = df4b, group = "group", limit.comp = "NEMATODE", min.n = 5, message = F, QC = "ignore", plot = T)
+or4bdf <- data.table::rbindlist(or4b$orthregs)
+or4bp <- or4b$plots[[3]]
+# Widmayer vs. LC50 2d INVERT
+or4b$plots[[1]]
+print(glue::glue("slope = {round(or4bdf[1]$orth.reg.slope, digits = 2)}, y-intercept = {round(or4bdf[1]$orth.reg.intercept, digits = 2)}, r^2 = {round(or4bdf[1]$orth.reg.r.squared, digits = 2)}"))
+# Widmayer vs. LC50 1d INVERT 
+or4b$plots[[3]]
+print(glue::glue("slope = {round(or4bdf[3]$orth.reg.slope, digits = 2)}, y-intercept = {round(or4bdf[3]$orth.reg.intercept, digits = 2)}, r^2 = {round(or4bdf[3]$orth.reg.r.squared, digits = 2)}"))
+# Boyd vs. LC50 2d INVERT
+or4b$plots[[9]]
+print(glue::glue("slope = {round(or4bdf[9]$orth.reg.slope, digits = 2)}, y-intercept = {round(or4bdf[9]$orth.reg.intercept, digits = 2)}, r^2 = {round(or4bdf[9]$orth.reg.r.squared, digits = 2)}"))
+# Boyd vs. LC50 1d INVERT 
+or4b$plots[[11]]
+print(glue::glue("slope = {round(or4bdf[11]$orth.reg.slope, digits = 2)}, y-intercept = {round(or4bdf[11]$orth.reg.intercept, digits = 2)}, r^2 = {round(or4bdf[11]$orth.reg.r.squared, digits = 2)}"))
+# run the orth regressions across all pairs with QC filter, makes it worse for Boyd again.
+
 
 # temp export for scott
 #cowplot::ggsave2(or3cp, filename = "plots/Nematode-EC10-2d-Growth_Invert-LC50-1d-Mortality.png", width = 6, height = 6)
@@ -103,24 +125,43 @@ print(glue::glue("slope = {round(or3cdf[2]$orth.reg.slope, digits = 2)}, y-inter
 #===========================================================#
 # 4d POSSIBLY CUT 
 # Widmayer vs ALGAE 1 d, 2d, 3d, 4d possible and there are not clear guidelines can test all?
-# Try to merge obvious mrotality catagories (population?)
+# Try to merge obvious mortality categories (population?)
 # Same issue with EC50 and LC50, and IC50
 # update scott
-df3d <- dat %>%
-  dplyr::filter(group == "NEMATODE" | group == "ALGAE")
+df4c_test <- dat %>%
+  dplyr::filter(source %in% c("Widmayer et al. 2022", "Boyd et al. 2016", "EnviroTox DB")) %>%
+  dplyr::filter(group == "NEMATODE" | group == "ALGAE" | group == "NEMATODE_COPAS1") %>%
+  dplyr::filter(endpoint == "Population")
+table(df4c_test$test_statistic) # just pull the EC50 population, because it is by far the biggest group
+
+# filter it
+df4c <- dat %>%
+  dplyr::filter(source %in% c("Widmayer et al. 2022", "Boyd et al. 2016", "EnviroTox DB")) %>%
+  dplyr::filter(group == "NEMATODE" | group == "ALGAE" | group == "NEMATODE_COPAS1") %>%
+  dplyr::filter(case_when(group == "NEMATODE" ~ T,
+                          group == "NEMATODE_COPAS1" ~ T,
+                          source == "EnviroTox DB" & test_statistic == "EC50" & endpoint == "Population"  ~ T, 
+                          TRUE ~ F))
 
 # run the orth regressions across all pairs
-or3d <- pwOrthReg(data = df3d, group = "group", limit.comp = "NEMATODE", min.n = 3, message = F, plot = T)
-or3ddf <- data.table::rbindlist(or3d$orthregs)
-or3dp <- or3d$plots[[3]]
-print(glue::glue("slope = {round(or3ddf[3]$orth.reg.slope, digits = 2)}, y-intercept = {round(or3ddf[3]$orth.reg.intercept, digits = 2)}, r^2 = {round(or3ddf[3]$orth.reg.r.squared, digits = 2)}"))
-or3dp2 <- or3d$plots[[1]]
-or3dp3 <- or3d$plots[[4]]
+or4c <- pwOrthReg(data = df4c, group = "group", limit.comp = "NEMATODE", min.n = 3, message = F, QC = "ignore", plot = T)
+or4cdf <- data.table::rbindlist(or4c$orthregs)
+or4cp <- or4c$plots[[1]]
+#Widmayer vs. 1 day EC50 population
+or4c$plots[[1]]
+print(glue::glue("slope = {round(or4cdf[1]$orth.reg.slope, digits = 2)}, y-intercept = {round(or4cdf[1]$orth.reg.intercept, digits = 2)}, r^2 = {round(or4cdf[1]$orth.reg.r.squared, digits = 2)}"))
+
+# Boyd vs. 1 day EC50 population
+or4c$plots[[11]]
+print(glue::glue("slope = {round(or4cdf[11]$orth.reg.slope, digits = 2)}, y-intercept = {round(or4cdf[11]$orth.reg.intercept, digits = 2)}, r^2 = {round(or4cdf[11]$orth.reg.r.squared, digits = 2)}"))
 
 #===========================================================#
 # put them together
-figure3 <- cowplot::plot_grid(or3ap, or3bp, or3cp, or3dp, labels = c("A", "B", "C", "D"), align = "vh")
-cowplot::ggsave2(figure3, filename = "figures/figure3.png", width = 10, height = 10)
+fig4 <- cowplot::plot_grid(or4ap + labs(subtitle = ""),
+                           or4bp + labs(subtitle = ""),
+                           or4cp + labs(subtitle = ""),
+                           labels = c("A", "B", "C"), align = "vh", ncol = 1)
+cowplot::ggsave2(fig4, filename = glue::glue("figures/{today}_figure4.png"), width = 3, height = 9)
 
 #===========================================================#
 # Figure 4 - C. elegans toxicity data compared with common
